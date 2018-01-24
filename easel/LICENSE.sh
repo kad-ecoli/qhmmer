ea4136f50cf0b8514c1b49745e266f5407a0ca20
zcx <zcx@umich.edu>
2018-01-23 22:49:14 -0500

    initial commit

---
+++ easel/LICENSE.sh
@@ -0,0 +1,5 @@
+export EASEL_VERSION=0.2.snap20130225 
+export EASEL_DATE="February 2013"
+export EASEL_COPYRIGHT="Copyright (C) 2008 Howard Hughes Medical Institute"
+export EASEL_LICENSE="Freely distributed under the Janelia Farm Software License."
+
---
+++ easel/esl_msafile.c
@@ -0,0 +1,1 @@
+qesl_msafile.c
